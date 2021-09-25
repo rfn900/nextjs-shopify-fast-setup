3b0aa70cea80e61dad97b45d4da3be654770c53b
rfn900 <rfn900@gmail.com>
2021-09-25 16:57:05 +0200

    Setup next.js with for shopify dev with tailwind

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+  },
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+};
